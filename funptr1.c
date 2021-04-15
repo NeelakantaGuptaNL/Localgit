ef7ec37c8e4d6b561f193cecbd0a10ae4f8d298b
NeelakantaGuptaNL <neelguptanl@gmail.com>
2021-04-15 15:47:13 +0530

    first commit

---
+++ funptr1.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+void (*fun1)(void);
+void disp(void);
+
+int add(int a, int b);
+int sub(int a, int b);
+int div(int a, int b);
+int mul(int a, int b);
+
+int main()
+{
+int res=0;
+int (*op[4])(int n, int m)={add, sub, mul, div};
+
+res=(*op[0])(5,6);
+
+printf("res= %d\n",res);
+printf("res= %d\n",res);
+
+fun1=disp;
+fun1();
+return 0;
+}
+
+void disp(void)
+{
+  printf("disp fun\n");
+}
+
+int add(int a, int b)
+{
+  return a+b;
+}
+
+int sub(int a, int b)
+{
+  return a-b;
+}
+
+int mul(int a, int b)
+{
+  return a*b;
+}
+
+int div(int a, int b)
+{
+  return a/b;
+}
+
---
+++ funptr2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void fun1(){ printf("\n fun1");}
+void fun2(){ printf("\n fun2");}
+
+void funptr2(void (*fun)())
+{
+fun();
+}
+
+void main()
+{
+funptr2(fun1);
+funptr2(fun2);
+}
